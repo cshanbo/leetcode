ba53d0376bd4298d921a4ce46018ec9b96d226b3
Shanbo Cheng <cshanbo@gmail.com>
2016-07-29 14:58:24 +0800

    add

---
+++ 33.cpp
@@ -0,0 +1,57 @@
+//coding:utf-8
+/***********************************************************
+Program: Search in a rotated array
+Description: 
+Shanbo Cheng: cshanbo@gmail.com
+Date: 2016-07-29 14:13:10
+Last modified: 2016-07-29 14:18:19
+GCC version: 4.9.3
+***********************************************************/
+
+class Solution {
+    //firstly, binary search to find the rotation index, then binary search the two parts (could search only one too)
+    int findMin(vector<int>& nums, int l, int r){
+    	if(nums.size() == 1)
+            return 0;
+    	if (l < 0 || r < 0 || l >= nums.size() || r >= nums.size())
+    		return -1;
+    	int mid = l + (r - l) >> 1;
+    	if (l <= r){
+    		if (mid == 0)
+    			return nums[mid] > nums[mid + 1] ? mid + 1 : -1;
+    		if (mid == nums.size() - 1)
+    			return nums[mid] < nums[mid - 1] ? mid : -1;
+    		
+    		if (nums[mid] <= nums[mid - 1] && nums[mid] <= nums[mid + 1])
+    			return mid;
+    		else
+    			return max(findMin(nums, l, mid - 1), findMin(nums, mid + 1, r));
+    	}
+    	
+    	return -1;
+    }
+    
+    int binarySearch(vector<int>& nums, int l, int r, int& target){
+        int m = (l + r)/2;
+        while(l <= r){
+            if(nums[m] == target)
+                return m;
+            else if(nums[m] > target){
+                r = m - 1;
+                m = l + (r - l) >> 1;
+            }else{
+                l = m + 1;
+                m = l + (r - l) >> 1;
+            }
+        }
+        return -1;
+    }
+
+public:
+    int search(vector<int>& nums, int target) {
+        int index = findMin(nums, 0, nums.size() - 1);
+        if(index == -1)
+            return binarySearch(nums, 0, nums.size() - 1, target);
+        return max(binarySearch(nums, 0, index - 1, target), binarySearch(nums, index, nums.size() - 1, target));
+    }
+};
---
+++ 34.cpp
@@ -0,0 +1,81 @@
+//coding:utf-8
+/***********************************************************
+Program: Search for a range
+Description: 
+Shanbo Cheng: cshanbo@gmail.com
+Date: 2016-07-29 14:19:23
+Last modified: 2016-07-29 14:55:47
+GCC version: 4.9.3
+***********************************************************/
+
+#include <vector>
+#include <iostream>
+
+using namespace std;
+
+class Solution {
+    int binary(int l, int r, int target, vector<int>& nums) {
+        int m = l + ((r - l) >> 1);
+        while(l <= r) {
+            if(nums[m] == target)
+                return m;
+            else if(nums[m] > target) {
+                r = m - 1;
+                m = l + ((r - l) >> 1);
+            } else {
+                l = m + 1;
+                m = l + ((r - l) >> 1);
+            }
+        }
+        return -1;
+    }
+public:
+    vector<int> searchRange(vector<int>& nums, int target) {
+        int m = binary(0, nums.size() - 1, target, nums);
+        //didn't find
+        if(m == -1)
+            return vector<int> {-1, -1};
+        //search left
+        int l = 0, index = 0, r = m, mid = l + ((r - l) >> 1);
+        if(nums[index] != target) {
+            while(l <= r) {
+                if(mid > 0 && nums[mid] == target && nums[mid - 1] != target) {
+                    index = mid;
+                    break;
+                } else if (nums[mid] < target){
+                    l = mid + 1;
+                    mid = l + ((r - l) >> 1);
+                } else if(mid > 0 && nums[mid] == target && nums[mid - 1] == target) {
+                    r = mid - 1;
+                    mid = l + ((r - l) >> 1);
+                }
+            }
+        }
+        int left = index;
+        //search right
+        l = m, index = nums.size() - 1, r = nums.size() - 1, mid = l + ((r - l) >> 1);
+        if(nums[index] != target) {
+            while(l <= r) {
+                if(mid < nums.size() - 1 && nums[mid] == target && nums[mid + 1] != target) {
+                    index = mid;
+                    break;
+                } else if(nums[mid] > target) {
+                    r = mid - 1;
+                    mid = l + ((r - l) >> 1);
+                } else if(mid < nums.size() - 1 && nums[mid + 1] == target) {
+                    l = mid + 1;
+                    mid = l + ((r - l) >> 1);
+                }
+            }
+        }
+        int right = index; 
+        return vector<int> {left, right};
+    }
+};
+
+int main() {
+    vector<int> vec{1, 2};
+    int target = 3;
+    Solution slt;
+    cout << slt.searchRange(vec, target) << endl;
+}

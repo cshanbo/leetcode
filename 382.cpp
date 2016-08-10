f915e770f20d345e830d74c7f34ee91b283c9cf4
Shanbo Cheng <cshanbo@gmail.com>
2016-08-10 10:32:08 +0800

    Linked List Random Node

---
+++ 382.cpp
@@ -0,0 +1,38 @@
+//coding:utf-8
+/***********************************************************
+Program:  382
+Description: 
+Shanbo Cheng: cshanbo@gmail.com
+Date: 2016-08-10 10:30:57
+Last modified: 2016-08-10 10:31:45
+GCC version: 4.9.3
+***********************************************************/
+// the idea is Reservoir sampling
+// https://en.wikipedia.org/wiki/Reservoir_sampling
+class Solution {
+    ListNode* flag;
+public:
+    /** @param head The linked list's head. Note that the head is guanranteed to be not null, so it contains at least one node. */
+    Solution(ListNode* head) {
+        flag = head;
+    }
+    
+    /** Returns a random node's value. */
+    int getRandom() {
+        ListNode* node = flag;
+        int ret = node->val;
+        for(int i = 1; node; ++i) {
+            if(rand() % i == 0)
+                ret = node->val;
+            node = node->next;
+        }
+        return ret;
+    }
+};
+
+/**
+ * Your Solution object will be instantiated and called as such:
+ * Solution obj = new Solution(head);
+ * int param_1 = obj.getRandom();
+ */
+

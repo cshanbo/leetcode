b111ec5562514899b679b0216ea90cb82023fd1f
Shanbo Cheng <cshanbo@gmail.com>
2016-08-10 14:07:22 +0800

    recover binary search tree

---
+++ 99.cpp
@@ -0,0 +1,47 @@
+//coding:utf-8
+/***********************************************************
+Program: 
+Description: 
+Shanbo Cheng: cshanbo@gmail.com
+Date: 2016-08-10 14:03:25
+Last modified: 2016-08-10 14:06:53
+GCC version: 4.9.3
+***********************************************************/
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+
+//inorder traverse, the first element is the one bigger than the successor 
+//second element is the one smaller the previous ones
+//
+class Solution {
+    TreeNode* first;
+    TreeNode* second;
+    TreeNode* prev;
+public:
+    void recoverTree(TreeNode* root) {
+        prev = new TreeNode(INT_MIN);
+        traverse(root);
+        swap(first->val, second->val);
+    }
+    
+    void traverse(TreeNode* root) {
+        if(!root)
+            return;
+        traverse(root->left);
+        if (!first && prev->val >= root->val)
+            first = prev;
+    
+        if (first && prev->val >= root->val)
+            second = root;
+            
+        prev = root;
+        traverse(root->right);
+    }
+};

e89047459da8b3e6b310fc3524a9ee3cf8e9de57
Shanbo Cheng <cshanbo@gmail.com>
2016-08-17 16:25:01 +0800

    rename

--- 46.permutations.cpp
+++ 46.permutations.cpp
@@ -4,7 +4,7 @@ Program: Permutations
 Description: 
 Shanbo Cheng: cshanbo@gmail.com
 Date: 2016-08-02 09:30:16
-Last modified: 2016-08-02 10:02:19
+Last modified: 2016-08-17 16:21:40
 GCC version: 4.7.3
 std = C++ 11
 ******************************************/
@@ -13,7 +13,6 @@ std = C++ 11
 #include <iostream>
 using namespace std;
 
-//交换元素
 class Solution {
     void helper(vector<vector<int>>& ret, vector<int>& nums, vector<int>& one, int idx) {
         if(idx >= nums.size()) {

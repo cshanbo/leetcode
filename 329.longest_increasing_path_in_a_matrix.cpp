99f2c6744de9441facb39e82eeba5f183e13a9eb
Shanbo Cheng <cshanbo@gmail.com>
2016-08-18 19:17:05 +0800

    rename

--- 329.longest_increasing_path_in_a_matrix.cpp
+++ 329.longest_increasing_path_in_a_matrix.cpp
@@ -4,7 +4,7 @@ Program: Longest increasing path in a matrix
 Description: 
 Author: cshanbo@gmail.com
 Date: 2016-08-18 16:02:43
-Last modified: 2016-08-18 16:53:57
+Last modified: 2016-08-18 19:16:51
 GCC version: 4.9.3
 *****************************************/
 
@@ -14,34 +14,42 @@ GCC version: 4.9.3
 
 using namespace std;
 
-bool max(int a, int b, int c, int d) {
-    return max(a, max(b, max(c, d)));
-}
-
 class Solution {
 public:
     int longestIncreasingPath(vector<vector<int>>& matrix) {
-        int ret = 0;
-        vector<vector<bool>> visited(matrix.size(), vector<bool>(matrix[0].size(), 0));
-        for(int i = 0; i < matrix.size(); i++) {
-            for(int j = 0; j < matrix[0].size(); j++) {
-                ret = max(ret, dfs(matrix, matrix[i][j], ret, visited, i, j));
-            }
-        }
-        
+        if(matrix.empty())
+            return 0;
+        vector<vector<int>> visited(matrix.size(), vector<int>(matrix[0].size(), 0));
+        int ret = 1;
+        for(int i = 0; i < matrix.size(); i++)
+            for(int j = 0; j < matrix[0].size(); j++)
+                ret = max(ret, dfs(matrix, matrix[i][j] - 1, visited, i, j));
         return ret;
     }
     
-    int dfs(vector<vector<int>>& matrix, int cur, int ret, vector<vector<bool>>& visited, int i, int j) {
-        if(i < 0 || i >= matrix.size() || j < 0 || j >= matrix[0].size() || visited[i][j] || cur >= matrix[i][j])
-            return ret;
-        int r = ret + 1;
-        int temp = ret;
-        visited[i][j] = true;
-        return max(dfs(matrix, cur, r, visited, i + 1, j), 
-                    dfs(matrix, cur, r, visited, i - 1, j), 
-                    dfs(matrix, cur, r, visited, i, j + 1), 
-                    dfs(matrix, cur, r, visited, i, j + 1));
+    int dfs(vector<vector<int>>& matrix, int cur, vector<vector<int>>& visited, int i, int j) {
+        if(visited[i][j] != 0)
+            return visited[i][j];
+        cur = matrix[i][j];
+        int x, y, ret = 1;
+        x = i + 1;
+        y = j;
+        if(!(x < 0 || x >= matrix.size() || y < 0 || y >= matrix[0].size() || cur >= matrix[x][y]))
+            ret = max(ret, 1 + dfs(matrix, cur, visited, x, y));
+        x = i - 1;
+        y = j;
+        if(!(x < 0 || x >= matrix.size() || y < 0 || y >= matrix[0].size() || cur >= matrix[x][y]))
+            ret = max(ret, 1 + dfs(matrix, cur, visited, x, y));
+        x = i;
+        y = j + 1;
+        if(!(x < 0 || x >= matrix.size() || y < 0 || y >= matrix[0].size() || cur >= matrix[x][y]))
+            ret = max(ret, 1 + dfs(matrix, cur, visited, x, y));
+        x = i;
+        y = j - 1;
+        if(!(x < 0 || x >= matrix.size() || y < 0 || y >= matrix[0].size() || cur >= matrix[x][y]))
+            ret = max(ret, 1 + dfs(matrix, cur, visited, x, y));
+        visited[i][j] = ret; 
+        return visited[i][j];
     }
 };
 

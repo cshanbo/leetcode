dbf6c19ae51624a62be199549ace4ccd95cc4c43
Shanbo Cheng <cshanbo@gmail.com>
2016-08-29 16:31:55 +0800

    rename

---
+++ 318.maximum_product_of_word_length.cpp
@@ -0,0 +1,18 @@
+//coding:utf-8
+/*****************************************
+Program: maximum product of word lengths
+Description: 
+Author: cshanbo@gmail.com
+Date: 2016-08-29 16:31:13
+Last modified: 2016-08-29 16:31:48
+GCC version: 4.9.3
+*****************************************/
+
+#include <vector>
+
+class Solution {
+public:
+    int maxProduct(vector<string>& words) {
+        
+    }
+};

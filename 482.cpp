cae96a010eac8f4182c9fcd5c50370429c7f427b
Shanbo Cheng <cshanbo@gmail.com>
2017-03-25 16:41:19 +0800

    gitignore and 482

--- 482.cpp
+++ 482.cpp
@@ -4,22 +4,22 @@ Program: License key formatting
 Description: 
 Shanbo Cheng: cshanbo@gmail.com
 Date: 2017-03-25 16:06:37
-Last modified: 2017-03-25 16:06:55
+Last modified: 2017-03-25 16:40:34
 GCC version: 4.9.3
 ***********************************************************/
 
 class Solution {
 public:
     string licenseKeyFormatting(string S, int K) {
-        string res;
-        for (auto i = S.rbegin(); i < S.rend(); i++)
-            if (*i != '-') { // ignore '-' in original string
-                if (res.size() % (K+1) == K) 
-                    res += '-'; // every (K+1)th char is '-' from tail
-                res += toupper(*i);
+        string res = "";
+        for(int i = S.size() - 1; i >= 0; --i) {
+            if(S[i] != '-') {
+                if(res.length() % (K + 1) == K)
+                    res = "-" + res;
+                res = char(toupper(S[i])) + res;
             }
-        
-        reverse(res.begin(), res.end());
+        }
         return res;
     }
 };
+
